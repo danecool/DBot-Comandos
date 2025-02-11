16f2fc1ee6b5eae1266999c316139b6cef1d1b12
Daniel N. Coelho <55884542+danecool@users.noreply.github.com>
2025-02-10 22:36:36 -0300

    Update types.d.ts

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,6 @@
 declare module 'node-webpmux';
 declare module 'pretty-num';
-declare module '@victorsouzaleal/brasileirao';
+declare module '@danecool/brasileirao';
 declare module 'format-duration-time';
 declare module 'instagram-url-direct';
 declare module 'fb-downloader-scrapper';
